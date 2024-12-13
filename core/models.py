0c67a0a659758ba2d667ee06c707481d53715f66
AntoineMLD <antoine.moulard@hotmail.com>
2024-12-13 19:08:08 +0100

    installation de scrapy et d'un spider

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
+from django.db import models
 
+class StagingData(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    source_url = models.URLField()
+    scraped_at = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
