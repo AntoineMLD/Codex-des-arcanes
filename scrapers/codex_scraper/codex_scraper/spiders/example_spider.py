d8fb6abb2c5d67a02603e198dba98298fcf5f10d
AntoineMLD <antoine.moulard@hotmail.com>
2024-12-13 19:16:23 +0100

    Fix Flake8 formatting issues

--- scrapers/codex_scraper/codex_scraper/spiders/example_spider.py
+++ scrapers/codex_scraper/codex_scraper/spiders/example_spider.py
@@ -1,11 +1,12 @@
 import scrapy
 
+
 class ExampleSpider(scrapy.Spider):
-    name = 'example_spider'
-    allowed_domains = ['aidedd.org']
-    start_urls = ['https://www.aidedd.org']
+    name = "example_spider"
+    allowed_domains = ["aidedd.org"]
+    start_urls = ["https://www.aidedd.org"]
 
     def parse(self, response):
         # Exemple : Extraire des titres
-        for title in response.css('h1::text'):
-            yield {'title': title.get()}
+        for title in response.css("h1::text"):
+            yield {"title": title.get()}
