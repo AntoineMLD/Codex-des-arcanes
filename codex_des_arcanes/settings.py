ab381c8d4271d59b86acbb62df55959d6a88cf25
AntoineMLD <antoine.moulard@hotmail.com>
2024-12-14 20:14:22 +0100

    setting db test

--- codex_des_arcanes/settings.py
+++ codex_des_arcanes/settings.py
@@ -79,18 +79,22 @@ WSGI_APPLICATION = "codex_des_arcanes.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'codex_db'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-        'TEST': {
-    'NAME': 'test_codex_db',
-    'DEPENDENCIES': [],
-}}},
+DATABASES = (
+    {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME", "codex_db"),
+            "USER": os.getenv("DB_USER", "postgres"),
+            "PASSWORD": os.getenv("DB_PASSWORD", ""),
+            "HOST": os.getenv("DB_HOST", "localhost"),
+            "PORT": os.getenv("DB_PORT", "5432"),
+            "TEST": {
+                "NAME": "test_codex_db",
+                "DEPENDENCIES": [],
+            },
+        }
+    },
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
